8ebdef35d1d93d830efab71076c974f8e6859ed1
Damon.Liao <damon09273@gmail.com>
2017-06-23 23:11:22 +0800

    adjust pixel

--- pixel.js
+++ pixel.js
@@ -1,5 +1,4 @@
 // base code
-
 <!-- Facebook Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -10,14 +9,16 @@ document,'script','https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '557199714403315');
 fbq('track', 'PageView');
 </script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=557199714403315&ev=PageView&noscript=1"
-/></noscript>
 <!-- DO NOT MODIFY -->
 <!-- End Facebook Pixel Code -->
 
 
-//
+// event -- Puchase
+<script>
+  fbq('track', 'Purchase', {value: {{換成訂單金額的GTM變數名稱}}, currency:'TWD'});
+</script>
+
+// event -- custom event
 <script>
   fbq('track', 'viewedVideo', {
   url: {{換成影片URL的GTM變數名稱}},
@@ -33,9 +34,4 @@ src="https://www.facebook.com/tr?id=557199714403315&ev=PageView&noscript=1"
 // event -- addToCart
 <script>
   fbq('track', 'AddToCart');
-</script>
-
-// event -- Puchase
-<script>
-  fbq('track', 'Purchase', {value: {{換成訂單金額的GTM變數名稱}}, currency:'TWD'});
 </script>
