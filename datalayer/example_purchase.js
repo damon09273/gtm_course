85c5ec4351c2aa819a26fe5cf051640e93eb53e1
damon09273@gmail.com <damon09273@gmail.com>
2019-08-25 22:12:11 +0800

    Update example_purchase.js

--- datalayer/example_purchase.js
+++ datalayer/example_purchase.js
@@ -1,8 +1,8 @@
 <script>
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
- 'event': 'purchase',
- 'transactionId': 'abc-1',
- 'transactionTotal': 200
- });
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+   'event': 'purchase',
+   'transactionId': 'abc-1',
+   'transactionTotal': 200
+   });
 </script>
